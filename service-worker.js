eefb25129b9770afa75db01fd5fa3b76dc296ea7
Keyur Patel <simkeyur@gmail.com>
2025-02-01 00:33:44 -0600

    initial commit

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'jsonic-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
